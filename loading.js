419ea1faa2e66a3290e099181bc1968de0586d10
biolet16 <violet2677@naver.com>
2020-11-24 20:39:12 +0900

    임시버튼 추가

--- loading.js
+++ loading.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-export default function Loading() {
-    return (
-      <View style={styles.container}>
-        <Image style={styles.iconImg}
-        source={require('./icon/iconM.png')}/>
-        <View>
-            <Text style={styles.title}>글구멍</Text>
-            <Text style={styles.subTitle}>글을 이해하는 지혜</Text>
-        </View>
-      </View>
-    );
+
+export default class Loading extends Component {
+    moveMainPage(){
+           alert("이동");
+    }
+    render(){
+         return (
+           <View style={styles.container}>
+             <View>
+                 <Image style={styles.iconImg}
+                 source={require('./icon/iconM.png')}/>
+             </View>
+             <View>
+                 <Text style={styles.title}>글구멍</Text>
+                 <Text style={styles.subTitle}>글을 이해하는 지혜</Text>
+             </View>
+             <View style={styles.btnContainer}>
+                 <View style={styles.eachBtn}>
+                     <Button
+                       title="로그인"
+                       color="#C0C0C0"
+                     />
+                 </View>
+                 <View style={styles.eachBtn}>
+                     <Button
+                       onPress={this.moveMainPage}
+                       title="캘린더"
+                       color="#C0C0C0"
+                     />
+                 </View>
+                 <View style={styles.eachBtn}>
+                     <Button
+                       title="책검색"
+                       color="#C0C0C0"
+                     />
+                 </View>
+             </View>
+           </View>
+         );
+     }
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -22,7 +52,7 @@ const styles = StyleSheet.create({
     iconImg:{
          height:'25%',
          width:'50%',
-         padding:50
+         padding:100,
     },
     title:{
          fontWeight: "bold",
@@ -30,8 +60,15 @@ const styles = StyleSheet.create({
          marginTop:20
     },
     subTitle:{
-         marginTop:5,
-         fontSize: 10,
-    }
-
+         marginTop:1,
+         fontSize: 11,
+         textDecorationLine: "underline",
+    },
+    btnContainer:{
+         marginTop:50,
+         flexDirection: "row"
+    },
+    eachBtn:{
+         marginLeft:10,
+    },
 })
