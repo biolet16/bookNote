0a32627bfc034a37af8cd5d76feb5bbea750e592
biolet16 <violet2677@naver.com>
2020-12-04 18:17:06 +0900

    db 데이터 화면에 뿌리기

--- container/MemoPage.js
+++ container/MemoPage.js
@@ -14,7 +14,8 @@ export default function MemoPage ({navigation}){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+//        justifyContent: "center",
+//        alignItems: "center",
+        backgroundColor:'#ffffff',
     },
 })
--- store/BookNoteStore.js
+++ store/BookNoteStore.js
@@ -1,11 +1,19 @@
 import { observable, action, makeObservable, computed } from 'mobx';
+import moment from 'moment';
 import firestore from '@react-native-firebase/firestore';
 
 class BookNoteStore{
 
     //선택한 날짜 데이터 state 변수-변수앞에 무조건 @observable 붙여야됨(안붙이면 연결 안됨)
     @observable selectedDate = null;
+    //책 리스트
+    @observable bookList = [];
+    //이 달의 책 리스트
     @observable monthBookList = [];
+    //선택된 달
+    @observable currentMonth = moment().format('MM');
+    //임시 아이디
+    @observable userId = 'bora';
 
     //건드리지마
     constructor() {
@@ -18,6 +26,12 @@ class BookNoteStore{
         this.selectedDate=dayData;
     }
 
+    @action.bound
+    changeSelectMon(monData) {
+        this.currentMonth=monData;
+        this.findMonBookList(monData,this.bookList);
+    }
+
     //firebase DB 데이터 가져오는 예시 함수(월간 책 리스트 가져오는 함수)
     async getMonthBookList(){
         let list=[];
@@ -28,13 +42,27 @@ class BookNoteStore{
           .then(doc => {
             //doc._docs:DB데이터리스트 data._data:DB데이터
             doc._docs.map(data => {
-             console.log(data._data);
-             list.push(data._data);
+//             console.log(data._data);
+                list.push(data._data);
             });
             //store state 데이터 매핑
-            this.monthBookList=list;
-            console.log(this.monthBookList);
+            this.bookList=list;
+            this.findMonBookList(this.currentMonth,this.bookList);
+//            console.log(this.bookList);
           });
+    }
+    @action.bound
+    findMonBookList(monData,bookList){
+        let list=[];
+        bookList.map(data => {
+           let dataMonth=moment(data.DATE).format('MM');
+           console.log(this.userId, data.ID, monData, dataMonth);
+           if(this.userId===data.ID && monData===dataMonth){
+              list.push(data);
+           }
+        });
+        this.monthBookList=list;
+        console.log('findMonBookList',this.monthBookList);
     }
 
     //선택 날짜 저장 함수(@action.bound 함수마다 무조건 붙이기)
